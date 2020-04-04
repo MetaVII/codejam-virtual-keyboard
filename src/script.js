d2f979753f12455095d23445dc3c04ed761048ad
Alex <alex.kryzhny@mail.ru>
2020-04-04 15:23:11 +0300

    feat: add language, shift, caps lock handling

---
+++ src/script.js
@@ -0,0 +1,613 @@
+const keyList = [
+  {
+    className: 'keyboard__row',
+    keys: [
+      {
+        eng: '`',
+        rus: 'ё',
+        shiftEng: '~',
+        shiftRus: 'Ё',
+        className: 'keyboard__key',
+        code: 'Backquote',
+      },
+      {
+        eng: '1',
+        shiftEng: '!',
+        className: 'keyboard__key',
+        code: 'Digit1',
+      },
+      {
+        eng: '2',
+        shiftEng: '@',
+        className: 'keyboard__key',
+        code: 'Digit2',
+      },
+      {
+        eng: '3',
+        shiftEng: '#',
+        className: 'keyboard__key',
+        code: 'Digit3',
+      },
+      {
+        eng: '4',
+        shiftEng: '$',
+        className: 'keyboard__key',
+        code: 'Digit4',
+      },
+      {
+        eng: '5',
+        shiftEng: '%',
+        className: 'keyboard__key',
+        code: 'Digit5',
+      },
+      {
+        eng: '6',
+        shiftEng: '^',
+        className: 'keyboard__key',
+        code: 'Digit6',
+      },
+      {
+        eng: '7',
+        shiftEng: '&',
+        className: 'keyboard__key',
+        code: 'Digit7',
+      },
+      {
+        eng: '8',
+        shiftEng: '*',
+        className: 'keyboard__key',
+        code: 'Digit8',
+      },
+      {
+        eng: '9',
+        shiftEng: '(',
+        className: 'keyboard__key',
+        code: 'Digit9',
+      },
+      {
+        eng: '0',
+        shiftEng: ')',
+        className: 'keyboard__key',
+        code: 'Digit0',
+      },
+      {
+        eng: '-',
+        shiftEng: '_',
+        className: 'keyboard__key',
+        code: 'Minus',
+      },
+      {
+        eng: '=',
+        shiftEng: '+',
+        className: 'keyboard__key',
+        code: 'Equal',
+      },
+      {
+        eng: 'Backspace',
+        className:
+          'keyboard__key keyboard__key--widest keyboard__key--secondary-color keyboard__key--bold',
+        code: 'Backspace',
+      },
+    ],
+  },
+  {
+    className: 'keyboard__row',
+    keys: [
+      {
+        eng: 'Tab',
+        className:
+          'keyboard__key keyboard__key--secondary-color keyboard__key--bold keyboard__key--wide',
+        code: 'Tab',
+      },
+      {
+        eng: 'q',
+        rus: 'й',
+        shiftEng: 'Q',
+        shiftRus: 'Й',
+        className: 'keyboard__key',
+        code: 'KeyQ',
+      },
+      {
+        eng: 'w',
+        rus: 'ц',
+        shiftEng: 'W',
+        shiftRus: 'Ц',
+        className: 'keyboard__key',
+        code: 'KeyW',
+      },
+      {
+        eng: 'e',
+        rus: 'у',
+        shiftEng: 'E',
+        shiftRus: 'У',
+        className: 'keyboard__key',
+        code: 'KeyE',
+      },
+      {
+        eng: 'r',
+        rus: 'к',
+        shiftEng: 'R',
+        shiftRus: 'К',
+        className: 'keyboard__key',
+        code: 'KeyR',
+      },
+      {
+        eng: 't',
+        rus: 'е',
+        shiftEng: 'T',
+        shiftRus: 'Е',
+        className: 'keyboard__key',
+        code: 'KeyT',
+      },
+      {
+        eng: 'y',
+        rus: 'н',
+        shiftEng: 'Y',
+        shiftRus: 'Н',
+        className: 'keyboard__key',
+        code: 'KeyY',
+      },
+      {
+        eng: 'u',
+        rus: 'г',
+        shiftEng: 'U',
+        shiftRus: 'Г',
+        className: 'keyboard__key',
+        code: 'KeyU',
+      },
+      {
+        eng: 'i',
+        rus: 'ш',
+        shiftEng: 'I',
+        shiftRus: 'Ш',
+        className: 'keyboard__key',
+        code: 'KeyI',
+      },
+      {
+        eng: 'o',
+        rus: 'щ',
+        shiftEng: 'O',
+        shiftRus: 'Щ',
+        className: 'keyboard__key',
+        code: 'KeyO',
+      },
+      {
+        eng: 'p',
+        rus: 'з',
+        shiftEng: 'P',
+        shiftRus: 'З',
+        className: 'keyboard__key',
+        code: 'KeyP',
+      },
+      {
+        eng: '[',
+        rus: 'х',
+        shiftEng: '{',
+        shiftRus: 'Х',
+        className: 'keyboard__key',
+        code: 'BracketLeft',
+      },
+      {
+        eng: ']',
+        rus: 'ъ',
+        shiftEng: '}',
+        shiftRus: 'Ъ',
+        className: 'keyboard__key',
+        code: 'BracketRight',
+      },
+      {
+        eng: '\\',
+        shiftEng: '|',
+        className: 'keyboard__key',
+        code: 'Backslash',
+      },
+      {
+        eng: 'Del',
+        className:
+          'keyboard__key keyboard__key--secondary-color keyboard__key--bold keyboard__key--wide',
+        code: 'Delete',
+      },
+    ],
+  },
+  {
+    className: 'keyboard__row',
+    keys: [
+      {
+        eng: 'CapsLock',
+        className:
+          'keyboard__key keyboard__key--widest keyboard__key--secondary-color keyboard__key--bold',
+        code: 'CapsLock',
+      },
+      {
+        eng: 'a',
+        rus: 'ф',
+        shiftEng: 'A',
+        shiftRus: 'Ф',
+        className: 'keyboard__key',
+        code: 'KeyA',
+      },
+      {
+        eng: 's',
+        rus: 'ы',
+        shiftEng: 'S',
+        shiftRus: 'Ы',
+        className: 'keyboard__key',
+        code: 'KeyS',
+      },
+      {
+        eng: 'd',
+        rus: 'в',
+        shiftEng: 'D',
+        shiftRus: 'В',
+        className: 'keyboard__key',
+        code: 'KeyD',
+      },
+      {
+        eng: 'f',
+        rus: 'а',
+        shiftEng: 'F',
+        shiftRus: 'А',
+        className: 'keyboard__key',
+        code: 'KeyF',
+      },
+      {
+        eng: 'g',
+        rus: 'п',
+        shiftEng: 'G',
+        shiftRus: 'П',
+        className: 'keyboard__key',
+        code: 'KeyG',
+      },
+      {
+        eng: 'h',
+        rus: 'р',
+        shiftEng: 'H',
+        shiftRus: 'Р',
+        className: 'keyboard__key',
+        code: 'KeyH',
+      },
+      {
+        eng: 'j',
+        rus: 'о',
+        shiftEng: 'J',
+        shiftRus: 'О',
+        className: 'keyboard__key',
+        code: 'KeyJ',
+      },
+      {
+        eng: 'k',
+        rus: 'л',
+        shiftEng: 'K',
+        shiftRus: 'Л',
+        className: 'keyboard__key',
+        code: 'KeyK',
+      },
+      {
+        eng: 'l',
+        rus: 'д',
+        shiftEng: 'L',
+        shiftRus: 'Д',
+        className: 'keyboard__key',
+        code: 'KeyL',
+      },
+      {
+        eng: ';',
+        rus: 'ж',
+        shiftEng: ':',
+        shiftRus: 'Ж',
+        className: 'keyboard__key',
+        code: 'Semicolon',
+      },
+      {
+        eng: `'`,
+        rus: 'э',
+        shiftEng: '"',
+        shiftRus: 'Э',
+        className: 'keyboard__key',
+        code: 'Quote',
+      },
+      {
+        eng: 'Enter',
+        className:
+          'keyboard__key keyboard__key--widest keyboard__key--secondary-color keyboard__key--bold',
+        code: 'Enter',
+      },
+    ],
+  },
+  {
+    className: 'keyboard__row',
+    keys: [
+      {
+        eng: 'Shift',
+        className:
+          'keyboard__key keyboard__key--widest keyboard__key--secondary-color keyboard__key--bold',
+        code: 'ShiftLeft',
+      },
+      {
+        eng: 'z',
+        rus: 'я',
+        shiftEng: 'Z',
+        shiftRus: 'Я',
+        className: 'keyboard__key',
+        code: 'KeyZ',
+      },
+      {
+        eng: 'x',
+        rus: 'ч',
+        shiftEng: 'X',
+        shiftRus: 'Ч',
+        className: 'keyboard__key',
+        code: 'KeyX',
+      },
+      {
+        eng: 'c',
+        rus: 'с',
+        shiftEng: 'C',
+        shiftRus: 'С',
+        className: 'keyboard__key',
+        code: 'KeyC',
+      },
+      {
+        eng: 'v',
+        rus: 'м',
+        shiftEng: 'V',
+        shiftRus: 'М',
+        className: 'keyboard__key',
+        code: 'KeyV',
+      },
+      {
+        eng: 'b',
+        rus: 'и',
+        shiftEng: 'B',
+        shiftRus: 'И',
+        className: 'keyboard__key',
+        code: 'KeyB',
+      },
+      {
+        eng: 'n',
+        rus: 'т',
+        shiftEng: 'N',
+        shiftRus: 'Т',
+        className: 'keyboard__key',
+        code: 'KeyN',
+      },
+      {
+        eng: 'm',
+        rus: 'ь',
+        shiftEng: 'M',
+        shiftRus: 'Ь',
+        className: 'keyboard__key',
+        code: 'KeyM',
+      },
+      {
+        eng: ',',
+        rus: 'б',
+        shiftEng: '<',
+        shiftRus: 'Б',
+        className: 'keyboard__key',
+        code: 'Comma',
+      },
+      {
+        eng: '.',
+        rus: 'ю',
+        shiftEng: '>',
+        shiftRus: 'Ю',
+        className: 'keyboard__key',
+        code: 'Period',
+      },
+      {
+        eng: '/',
+        rus: '.',
+        shiftEng: '?',
+        shiftRus: ',',
+        className: 'keyboard__key',
+        code: 'Slash',
+      },
+      {
+        eng: '&#8593;',
+        className: 'keyboard__key keyboard__key--secondary-color',
+        code: 'ArrowUp',
+      },
+      {
+        eng: 'Shift',
+        className:
+          'keyboard__key keyboard__key--widest keyboard__key--secondary-color keyboard__key--bold',
+        code: 'ShiftRight',
+      },
+    ],
+  },
+  {
+    className: 'keyboard__row',
+    keys: [
+      {
+        eng: 'Ctrl',
+        className:
+          'keyboard__key keyboard__key--secondary-color keyboard__key--bold',
+        code: 'ControlLeft',
+      },
+      {
+        eng: 'Win',
+        className:
+          'keyboard__key keyboard__key--secondary-color keyboard__key--bold',
+        code: 'MetaLeft',
+      },
+      {
+        eng: 'Alt',
+        className:
+          'keyboard__key keyboard__key--secondary-color keyboard__key--bold',
+        code: 'AltLeft',
+      },
+      {
+        eng: '',
+        className: 'keyboard__key keyboard__key--space',
+        code: 'Space',
+      },
+      {
+        eng: 'Alt',
+        className:
+          'keyboard__key keyboard__key--secondary-color keyboard__key--bold',
+        code: 'AltRight',
+      },
+      {
+        eng: '&#8592',
+        className: 'keyboard__key keyboard__key--secondary-color',
+        code: 'ArrowLeft',
+      },
+      {
+        eng: '&#8595',
+        className: 'keyboard__key keyboard__key--secondary-color',
+        code: 'ArrowDown',
+      },
+      {
+        eng: '&#8594',
+        className: 'keyboard__key keyboard__key--secondary-color',
+        code: 'ArrowRight',
+      },
+      {
+        eng: 'Ctrl',
+        className: 'keyboard__key keyboard__key--secondary-color',
+        code: 'ControlRight',
+      },
+    ],
+  },
+];
+
+(function setInitialLang() {
+  const lang = sessionStorage.getItem('lang');
+  if (!lang) {
+    sessionStorage.setItem('lang', 'eng');
+  }
+})();
+
+function appendElement(container, className) {
+  const ELEMENT = document.createElement('div');
+  ELEMENT.classList.add(...className.split(' '));
+  container.append(ELEMENT);
+  return ELEMENT;
+}
+
+const CONTAINER = document.createElement('div');
+CONTAINER.classList.add('keyboard__container');
+document.body.prepend(CONTAINER);
+
+// keyboard generation
+keyList.forEach((row) => {
+  const lang = sessionStorage.getItem('lang');
+  const { className, keys } = row;
+  const ROW = appendElement(CONTAINER, className);
+
+  keys.forEach((key) => {
+    const { className: keyClassName, eng, rus, code } = key;
+    const KEY = appendElement(ROW, keyClassName);
+    KEY.dataset.code = code;
+
+    if (lang === 'rus' && rus) {
+      KEY.innerHTML = rus;
+    } else {
+      KEY.innerHTML = eng;
+    }
+  });
+});
+
+const BUTTONS = document.querySelectorAll('.keyboard__key');
+
+// change language
+function changeLanguage(isCapsLock) {
+  const lang = sessionStorage.getItem('lang');
+  const buttonsArr = [...BUTTONS];
+
+  keyList.forEach((row) => {
+    const { keys } = row;
+
+    keys.forEach((key) => {
+      const { eng, rus, shiftEng, shiftRus, code } = key;
+      const button = buttonsArr.find((b) => b.dataset.code === code);
+      if (lang === 'eng') {
+        button.innerHTML = isCapsLock && shiftEng ? shiftEng : eng;
+      } else {
+        button.innerHTML = isCapsLock
+          ? shiftRus || shiftEng || eng
+          : rus || eng;
+      }
+    });
+  });
+}
+
+// shift handle
+function changeToShift(isPressed) {
+  const lang = sessionStorage.getItem('lang');
+  const buttonsArr = [...BUTTONS];
+
+  keyList.forEach((row) => {
+    const { keys } = row;
+
+    keys.forEach((key) => {
+      const { eng, rus, shiftEng, shiftRus, code } = key;
+      const button = buttonsArr.find((b) => b.dataset.code === code);
+      if (lang === 'eng') {
+        button.innerHTML = isPressed && shiftEng ? shiftEng : eng;
+      } else {
+        button.innerHTML = isPressed ? shiftRus || shiftEng || eng : rus || eng;
+      }
+    });
+  });
+}
+
+// button click handle
+function handleButtonDown(event) {
+  event.preventDefault();
+  const { code, key, ctrlKey, altKey, shiftKey } = event;
+  const isCapsLock = event.getModifierState('CapsLock');
+
+  if (ctrlKey && altKey) {
+    const lang = sessionStorage.getItem('lang');
+    if (lang === 'eng') {
+      sessionStorage.setItem('lang', 'rus');
+    } else {
+      sessionStorage.setItem('lang', 'eng');
+    }
+    changeLanguage(isCapsLock);
+  }
+
+  if (key === 'CapsLock') {
+    if (isCapsLock) {
+      changeToShift(true);
+    } else {
+      changeToShift(false);
+    }
+  }
+
+  if (shiftKey) {
+    if (isCapsLock) {
+      changeToShift(false);
+    } else {
+      changeToShift(true);
+    }
+  }
+
+  const button = [...BUTTONS].find((b) => b.dataset.code === code);
+  if (button) {
+    button.classList.add('keyboard__key--pressed');
+  }
+}
+
+function handleButtonUp(event) {
+  const { code, key } = event;
+  const isCapsLock = event.getModifierState('CapsLock');
+
+  if (key === 'Shift') {
+    if (isCapsLock) {
+      changeToShift(true);
+    } else {
+      changeToShift(false);
+    }
+  }
+
+  const button = [...BUTTONS].find((b) => b.dataset.code === code);
+  if (button && !(button.dataset.code === 'CapsLock' && isCapsLock)) {
+    button.classList.remove('keyboard__key--pressed');
+  }
+}
+
+document.addEventListener('keydown', handleButtonDown);
+document.addEventListener('keyup', handleButtonUp);
